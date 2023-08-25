674905d9e90f6bcd842585795633830ae11434cf
Glykosim <142170909+Glykosim@users.noreply.github.com>
2023-08-25 10:25:07 +0200

    Lagt ut innstillinger i egen fil

---
+++ config.js
@@ -0,0 +1,35 @@
+// config.js
+
+const VERDIRABATT = 0.7;
+const BUNNFRADRAG = 2500000;
+const PROMILLE = 0.001;
+const INNTEKTSGRENSE = 711720;
+const BILDE_NAVN = "Askøy_sv_gjennomsiktig.png";
+
+const INFO_TEXT = `
+    <h1>Hva får Askøy igjen for denne skatten?</h1>
+	
+	<ul style="font-size:24px;">
+    <li><b>Skolebygging på Erdal</b>
+	<ul>	Skolene på Erdal kan ikke vente. Bygging er veldig vanskelig uten eiendomsskatt.</ul></li>
+	<br>
+	<li><b>Gratis trygghetsalarmer</b>
+	<ul>	Dette koster ca. 3 000 kroner i leie per år. Trygghet skal ikke koste.</ul></li>
+    <br>
+	<li><b>Økt bemanning og vedlikehold i skoler og barnehager</b>
+	<ul>	Skolene rapporterer om uro, mobbing og dårlig trivsel. Sykefraværet i barnehagene er høyt. Vi trenger flere voksne! </ul></li>
+    <br>
+	<li><b>Frikort for helsetjenester og medisiner til dem med lavest inntekt</b>
+	<ul>	Dette vil bety at alle vil ha råd til medisiner og legebesøk.</ul></li>
+    <br>
+	<li><b>Nedbetaling på kommunens gjeld</b>
+	<ul>	Vi bruker i dag 200 millioner kroner på å betjene gjeld! Dette er altfor mye.</ul></li>
+	<br>
+	<li><b>Styrking av eldreomsorgen</b>
+	<ul>	Askøy klarer ikke å ta i mot utskrivningsklare pasienter og det koster millioner. Kapasiteten må bygges ut og det løses ved eiendomsskatt.</ul></li>
+	<br>
+	<li><b>Styrking av sosialsatsene</b>
+	<ul>	Husholdningene i Askøy tjener på nivå med rike Bærum. SV vil redusere forskjellene mellom hos oss.</ul></li>
+</ul>
+`
+
